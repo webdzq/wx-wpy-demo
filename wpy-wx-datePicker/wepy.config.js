694292a86e37f2f67255fcfccfe2f0f6b1143a5c
dongzhengqi <dzq258@126.com>
2017-05-25 11:14:31 +0800

    add files

---
+++ wpy-wx-datePicker/wepy.config.js
@@ -0,0 +1,27 @@
+var prod = process.env.NODE_ENV === 'production'
+
+module.exports = {
+    wpyExt: '.wpy',
+    eslint: true,
+    compilers: {
+        less: {
+            compress: true
+        },
+        /*sass: {
+          outputStyle: 'compressed'
+        },*/
+        babel: {
+            sourceMap: true,
+            presets: [
+                'es2015',
+                'stage-1'
+            ],
+            plugins: [
+                'transform-decorators-legacy',
+                'transform-export-extensions',
+                'syntax-export-extensions'
+            ]
+        }
+    },
+    plugins: {}
+}
